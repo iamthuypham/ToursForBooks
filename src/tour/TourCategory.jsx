06e8f15e72a983c0198329bcff2b7adfe3245e0f
iamthuypham <tpham045@hotmail.com>
2019-02-17 12:23:50 -0800

    Fix typo valunteer

--- src/tour/TourCategory.jsx
+++ src/tour/TourCategory.jsx
@@ -1,10 +1,7 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Avatar from '@material-ui/core/Avatar';
-import Chip from '@material-ui/core/Chip';
-import FaceIcon from '@material-ui/icons/Face';
-import DoneIcon from '@material-ui/icons/Done';
+import React from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import Chip from '@material-ui/core/Chip'
 
 const styles = theme => ({
   root: {
@@ -13,73 +10,78 @@ const styles = theme => ({
     // flexWrap: 'wrap',
   },
   chip: {
-    margin: theme.spacing.unit,
-  },
-});
+    margin: theme.spacing.unit
+  }
+})
 
-  class TourCategory extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state={
-         color: 'default',
-         active: 'none',
-      }
-    }    
-
-    ChipColor(position) {
-      if ( this.state.active === position) {
-        return "primary";
-      }
-      return "default";
-   }
-  
-    toggle(position){        
-      if (this.state.active === position) {
-        this.setState({active : 'none'},() =>{
-          this.props.handleCategory(this.state.active);
-        })
-      } else {
-        this.setState({active : position},() =>{
-          this.props.handleCategory(this.state.active);
-        })        
-      }
-      if(this.state.active === 'none') 
-          {
-             this.setState({active : position},() =>{
-              this.props.handleCategory(this.state.active);
-        })            
-          } 
+class TourCategory extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      color: 'default',
+      active: 'none'
+    }
   }
 
-  render  (){
-    const { classes } = this.props;
+  ChipColor(position) {
+    if (this.state.active === position) {
+      return 'primary'
+    }
+    return 'default'
+  }
+
+  toggle(position) {
+    if (this.state.active === position) {
+      this.setState({ active: 'none' }, () => {
+        this.props.handleCategory(this.state.active)
+      })
+    } else {
+      this.setState({ active: position }, () => {
+        this.props.handleCategory(this.state.active)
+      })
+    }
+    if (this.state.active === 'none') {
+      this.setState({ active: position }, () => {
+        this.props.handleCategory(this.state.active)
+      })
+    }
+  }
+
+  render() {
+    const { classes } = this.props
     return (
-    <div className={classes.root}>
-      <Chip
-        label="Commerical" 
-        color={this.ChipColor('Commerical')}
-        onClick={() => {this.toggle('Commerical')}} 
-        className={classes.chip} 
-      />  
-      <Chip
-        label="Valunteer"  
-        color={this.ChipColor('Valunteer')}
-        onClick={() => {this.toggle('Valunteer')}} 
-        className={classes.chip} 
-      />  
-      <Chip
-        label="EcoTour"  
-        color={this.ChipColor('EcoTour')}
-        onClick={() => {this.toggle('EcoTour')}} 
-        className={classes.chip} 
-      />  
-    </div>
+      <div className={classes.root}>
+        <Chip
+          label="Commerical"
+          color={this.ChipColor('Commerical')}
+          onClick={() => {
+            this.toggle('Commerical')
+          }}
+          className={classes.chip}
+        />
+        <Chip
+          label="Volunteer"
+          color={this.ChipColor('Volunteer')}
+          onClick={() => {
+            this.toggle('Volunteer')
+          }}
+          className={classes.chip}
+        />
+        <Chip
+          label="EcoTour"
+          color={this.ChipColor('EcoTour')}
+          onClick={() => {
+            this.toggle('EcoTour')
+          }}
+          className={classes.chip}
+        />
+      </div>
     )
   }
 }
 
 TourCategory.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
+  classes: PropTypes.object.isRequired
+}
 
-export default withStyles(styles)(TourCategory);
+export default withStyles(styles)(TourCategory)
